e03da2bcf4855658568ff452d7f82091586e07c5
edinsonv <bev4807@g.rit.edu>
2017-11-19 21:20:23 -0500

    Added background and sound assets.

---
+++ SuperUltraGunFight/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+	public int fps = 30;
+	public string currentAnim = "PlayerIdle";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
