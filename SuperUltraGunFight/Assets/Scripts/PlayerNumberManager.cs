00c2a9553a7da5eb43c9754659e31cb36b3f37db
Tung Nguyen <nguyenhaitungg@gmail.com>
2017-11-28 01:37:28 -0500

    Added a sample player 3. Added a basic Main Menu and basic Players Selection Scene.

---
+++ SuperUltraGunFight/Assets/Scripts/PlayerNumberManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNumberManager : MonoBehaviour
+{
+    public int playerNum;
+
+	public void NumSelect(int num)
+    {
+        playerNum = num;
+    }
+}
---
+++ SuperUltraGunFight/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text player1Score;
+    public Text player2Score;
+    public Text player3Score;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
