50f9791cbb37cdd5d9ce47bf133d9807f03e125d
Isaac Kelman <ikelman27@gmail.com>
2017-12-09 12:31:42 -0500

    Added singelton Gamemanager

---
+++ SuperUltraGunFight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+#region variables
+    public static GameManager instance = null;
+    private string[] levels = { };
+    private int currentLevel;
+#endregion
+
+    void Awake()
+    {
+        currentLevel = 0;
+        if (instance == null)
+        {
+            instance = this;
+           
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+
+
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    public void LoadNextScene()
+    {
+
+    }
+
+}
